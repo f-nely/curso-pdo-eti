15d63de37c9656a6ef92e8299d983507c1ed3f5b
nely de faria <nelly.nf54@gmail.com>
2021-11-09 16:08:03 -0300

    Criando arquivo exec.php

---
+++ public/exec.php
@@ -0,0 +1,28 @@
+<?php
+
+$dsn = 'mysql';
+$host = 'mysql'; // localhost
+$database = 'curso_php_pdo';
+$user = 'root';
+$password = 'root';
+$port = 3306;
+
+try {
+
+  $pdo = new PDO("{$dsn}:host={$host};port={$port};dbname={$database}", $user, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = "INSERT INTO products (name, description) VALUES ('Copo', 'Descartável')";
+  $insert = $pdo->exec($sql); 
+  var_dump($insert);
+
+  /* $name = 'Gravador';
+  $description = 'Portátil';
+  $update = $pdo->exec("UPDATE products SET name = '{$name}',description ='{$description}' WHERE id <> 5"); */
+
+  
+} catch (Throwable | PDOException $e) {
+
+  echo $e->getCode() . '<br>';
+  echo $e->getMessage();
+}
