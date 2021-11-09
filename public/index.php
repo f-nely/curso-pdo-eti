108071d887e932cc45e212d6c985eb1d9a91ac3a
nely de faria <nelly.nf54@gmail.com>
2021-11-09 15:25:27 -0300

    Atualizando dados no banco

--- public/index.php
+++ public/index.php
@@ -12,9 +12,13 @@ try {
   $pdo = new PDO("{$dsn}:host={$host};port={$port};dbname={$database}", $user, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = "INSERT INTO products (name, description) VALUES ('Copo', 'Descartável')";
-  $insert = $pdo->exec($sql);
-  var_dump($insert);
+  /* $sql = "INSERT INTO products (name, description) VALUES ('Copo', 'Descartável')";
+  $insert = $pdo->exec($sql); */
+
+  $name = 'Gravador';
+  $description = 'Portátil';
+  $update = $pdo->exec("UPDATE products SET name = '{$name}',description ='{$description}' WHERE id <> 5");
+  var_dump($update);
 
 } catch (Throwable | PDOException $e) {
 
